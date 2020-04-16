08ffa6948aaeae73c578c69aeb17b74e27bf983f
Yusril <yusril@createch.id>
2020-04-16 23:48:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/exercisesql_lite/ModKontak.java
@@ -0,0 +1,49 @@
+package com.example.exercisesql_lite;
+
+public class ModKontak {
+    private String idUsr;
+    private String name;
+    private String phone;
+    private String email;
+    private String alamat;
+
+    public String getIdUser() {
+        return idUsr;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUsr = idUser;
+    }
+
+    public String getNama() {
+        return name;
+    }
+
+    public void setNama(String nama) {
+        this.name = nama;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
